8bb9566d6956e3b8a5c330a3094c97bd8ee916c2
Co van Leeuwen <Coo@covle.com>
2017-04-30 00:00:32 +0800

    Added the BT Service

--- src/android/TaffPrint.java
+++ src/android/TaffPrint.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -15,37 +16,41 @@ public class TaffPrint extends CordovaPlugin {
 
     private final String TAG = "coo";
     private BluetoothAdapter mBtAdapter;
-    private ArrayList<String> mPairedDevices;
+    private JSONArray mPairedDevices;
+
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+    // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
+    // Member object for the services
+    private BluetoothService mService = null;
 
 
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        Log.d(TAG, "Something is happening");
-
         if (action.equals("scan")){
-            scan();
-
-
+            scan(callbackContext);
+        } else if (action.equals("connect")){
+            connect(data.getString(0), callbackContext);
         } else if (action.equals("greet")) {
-
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
-
         } else {
             return false;
         }
 
 
+        callbackContext.error("Unknown action");
         return true;
     }
 
-    public void scan(){
+    public void scan(CallbackContext callbackContext){
         // Get the local Bluetooth adapter
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
-        mPairedDevices = new ArrayList<String>();
+        mPairedDevices = new JSONArray();
 
         // Get a set of currently paired devices
         Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
@@ -53,14 +58,57 @@ public class TaffPrint extends CordovaPlugin {
         // If there are paired devices, add each one to the ArrayAdapter
         if (pairedDevices.size() > 0) {
             for (BluetoothDevice device : pairedDevices) {
-                mPairedDevices.add(device.getName() + "\n" + device.getAddress());
-                Log.d(TAG, device.getName());
+                JSONObject d = new JSONObject();
+
+                try{
+                    d.put("name", device.getName());
+                    d.put("address", device.getAddress());
+                    mPairedDevices.put(d);
+                } catch (Exception ex) {
+                    //cry
+                }
             }
         } else {
-            String noDevices = "No devices";
-            mPairedDevices.add(noDevices);
+            callbackContext.error("No paired devices");
+        }
+
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("devices", mPairedDevices);
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, json));
+        } catch (Exception ex){
+            callbackContext.error("Json error...");
         }
     }
 
+    private void init(){
+        if (mBluetoothAdapter == null){
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        }
+        if (mService == null) {
+            mService = new BluetoothService();
+        }
+    }
+
+    public void connect(String address, CallbackContext callback){
+        init();
+
+        if (mBluetoothAdapter == null){
+            callback.error("No bluetooth devide");
+        }
+
+        // Get the BluetoothDevice object
+        if (BluetoothAdapter.checkBluetoothAddress(address)) {
+            BluetoothDevice device = mBluetoothAdapter
+                    .getRemoteDevice(address);
+            // Attempt to connect to the device
+            mService.connect(device);
+
+            callback.success("Connecting to printer");
+        } else {
+            callback.error("Can't find printer");
+        }
+    }
 
 }
