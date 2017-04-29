5c8cb1450473014988d08e871038378bad7adfbe
Co van Leeuwen <Coo@covle.com>
2017-04-29 22:25:20 +0800

    Added skeleton for JS

--- www/TaffPrint.js
+++ www/TaffPrint.js
@@ -3,7 +3,24 @@ module.exports = {
         cordova.exec(successCallback, errorCallback, "TaffPrint", "greet", [name]);
     },
     scan: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "TaffPrint", "scan", [name]);
+        cordova.exec(successCallback, errorCallback, "TaffPrint", "scan", []);
+        //Should get a collection of printer names.
+    },
+    connect: function(name, success, error) {
+        console.log("Not implemented yet...");
+        cordova.exec(success, error, "TaffPrint", "connect", [name]);
+    },
+    print: function(message, success, error) {
+        console.log("Not implemented yet...");
+        cordova.exec(success, error, "TaffPrint", "print", [message]);
+    },
+    printLine: function(message, success, error) {
+        console.log("Not implemented yet...");
+        cordova.exec(success, error, "TaffPrint", "printLine", [message]);
+    },
+    printLogo: function(success, error) {
+        console.log("Not implemented yet...");
+        cordova.exec(success, error, "TaffPrint", "printLogo", []);
     }
+
 };
-// Taffprint in the separate plugin project
