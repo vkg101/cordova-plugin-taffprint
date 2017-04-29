f07a06980f529bbb63b10f71cd367429214ce885
Co van Leeuwen <Coo@covle.com>
2017-04-29 22:20:56 +0800

    some crude version that finds bluetooth devices

--- src/android/TaffPrint.java
+++ src/android/TaffPrint.java
@@ -1,26 +1,66 @@
 package com.covle.cordova.plugin;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.util.Log;
+
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class TaffPrint extends CordovaPlugin {
+
+    private final String TAG = "coo";
+    private BluetoothAdapter mBtAdapter;
+    private ArrayList<String> mPairedDevices;
+
+
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
+        Log.d(TAG, "Something is happening");
+
+        if (action.equals("scan")){
+            scan();
+
+
+        } else if (action.equals("greet")) {
 
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
 
-            return true;
-
         } else {
-
             return false;
+        }
 
+
+        return true;
+    }
+
+    public void scan(){
+        // Get the local Bluetooth adapter
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        mPairedDevices = new ArrayList<String>();
+
+        // Get a set of currently paired devices
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+
+        // If there are paired devices, add each one to the ArrayAdapter
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                mPairedDevices.add(device.getName() + "\n" + device.getAddress());
+                Log.d(TAG, device.getName());
+            }
+        } else {
+            String noDevices = "No devices";
+            mPairedDevices.add(noDevices);
         }
     }
+
+
 }
--- www/TaffPrint.js
+++ www/TaffPrint.js
@@ -1,5 +1,9 @@
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+        cordova.exec(successCallback, errorCallback, "TaffPrint", "greet", [name]);
+    },
+    scan: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "TaffPrint", "scan", [name]);
     }
 };
+// Taffprint in the separate plugin project
